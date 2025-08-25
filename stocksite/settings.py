1e23ec9754f64bb893d24a3f3157c30565cb9589
gorozooo <coco.louis67@gmail.com>
2025-08-25 21:51:38 +0900

    譖ｴ譁ｰ

--- stocksite/settings.py
+++ stocksite/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -6,7 +7,13 @@ SECRET_KEY = 'django-insecure-)l$5u7#*s5ls885avu*8rpmfiiiczle6vsr+y78%!cutwp%wpl
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gorozooo.com", "www.gorozooo.com", "192.168.1.16"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "gorozooo.com",
+    "www.gorozooo.com",
+    "192.168.1.16",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,7 +47,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'portfolio.context_processors.ui', 
+                'portfolio.context_processors.ui',
                 'portfolio.context_processors.bottom_tabs',
             ],
         },
@@ -68,16 +75,27 @@ TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_TZ = True
 
+# =============================
+# Static files
+# =============================
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# =============================
+# Default settings
+# =============================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-CSRF_TRUSTED_ORIGINS = ["http://192.168.1.16:8000""https://gorozooo.com",
+# =============================
+# Security settings
+# =============================
+CSRF_TRUSTED_ORIGINS = [
+    "http://192.168.1.16:8000",
+    "https://gorozooo.com",
     "https://www.gorozooo.com",
 ]
 
