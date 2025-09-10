8b2ca47681ec1addff5ae9ac97a41609eeb81ba3
gorozooo <coco.louis67@gmail.com>
2025-09-10 17:57:49 +0900

    views.py を更新

--- portfolio/views.py
+++ portfolio/views.py
@@ -29,6 +29,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.template.loader import get_template, render_to_string
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.dateparse import parse_date
 from django.views.decorators.cache import cache_page
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET, require_http_methods
