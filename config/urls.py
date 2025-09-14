1208624d3fa7cf67b90a5dadbb1243e73eadeca8
gorozooo <coco.louis67@gmail.com>
2025-09-15 00:12:59 +0900

    chore: add /healthz in root URLConf and include portfolio.urls if missing

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("healthz", lambda r: HttpResponse("ok")), 
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')), 
 ]
