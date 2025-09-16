ce943740ebd81fffd38bf6cc87d4306235909605
gorozooo <coco.louis67@gmail.com>
2025-09-16 21:31:12 +0900

    admin.py を更新

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,14 @@
+# portfolio/admin.py
 from django.contrib import admin
+from .models import Holding, UserSetting
 
-# Register your models here.
+@admin.register(Holding)
+class HoldingAdmin(admin.ModelAdmin):
+    list_display = ("user", "ticker", "quantity", "avg_cost", "updated_at")
+    search_fields = ("ticker", "name", "user__username")
+
+# すでにUserSetting登録済みならOK
+try:
+    admin.site.register(UserSetting)
+except Exception:
+    pass
