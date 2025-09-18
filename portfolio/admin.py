73ad0bb2019dd2ab6efa39574a14894719ac42bc
gorozooo <coco.louis67@gmail.com>
2025-09-18 22:29:20 +0900

    admin.py を更新

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,6 @@
 # portfolio/admin.py
 from django.contrib import admin
-from .models import Holding, UserSetting
+from .models import Holding, UserSetting, RealizedTrade
 
 @admin.register(Holding)
 class HoldingAdmin(admin.ModelAdmin):
@@ -12,3 +12,11 @@ try:
     admin.site.register(UserSetting)
 except Exception:
     pass
+    
+
+@admin.register(RealizedTrade)
+class RealizedTradeAdmin(admin.ModelAdmin):
+    list_display = ("trade_at", "ticker", "name", "broker", "side", "qty", "price", "fee", "tax", "cashflow", "pnl")
+    list_filter  = ("broker", "side", "trade_at")
+    search_fields= ("ticker", "name", "memo")
+    
