1cbc263a42c7f4e99156a2ed4d3ab763b83b6ba8
gorozooo <coco.louis67@gmail.com>
2025-09-21 13:55:06 +0900

    urls.py を更新

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -38,7 +38,9 @@ urlpatterns = [
     path("realized/summary-period", realized_views.summary_period_partial, name="realized_summary_period"),
     path("realized/chart-monthly.json", realized_views.chart_monthly_json, name="realized_chart_monthly"),
     path("realized/summary-partial/", realized_views.realized_summary_partial, name="realized_summary_partial"),
-
+    path("realized/realized_ranking_partial/", rankings_views.realized_ranking_partial, name="realized_ranking_partial"),
+    
+    
     # ← 追加：部分テンプレとCSV
     path("realized/partial/table", realized_views.table_partial, name="realized_table_partial"),
     path("realized/partial/summary", realized_views.summary_partial, name="realized_summary_partial"),
