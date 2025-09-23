f6cd95a6896b5680af62466d857a0c221b52ccf1
gorozooo <coco.louis67@gmail.com>
2025-09-23 09:36:01 +0900

    urls.py を更新

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,7 @@ from .views import core as core_views
 from .views import settings as settings_views
 from .views import api as api_views
 from .views import realized as realized_views
+from .views import dev_tools as dev_views
 
 urlpatterns = [
     path("", core_views.main, name="home"),
@@ -23,7 +24,10 @@ urlpatterns = [
 
     # 設定
     path("settings/trade", settings_views.trade_setting, name="trade_setting"),
-
+    
+    # デバグ
+    path("dev/scan-avg/", dev_views.scan_avg, name="scan_avg"),
+    
     # 保有
     path("holdings/", core_views.holdings_list, name="holdings_list"),
     path("holdings/<int:pk>/close", realized_views.close_sheet, name="holding_close_sheet"),  # 画面
