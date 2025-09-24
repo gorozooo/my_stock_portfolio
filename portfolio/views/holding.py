447590a1b2a566649e4f6ad80a2ed36fd3a17530
gorozooo <coco.louis67@gmail.com>
2025-09-24 21:05:01 +0900

    holding.py を更新

--- portfolio/views/holding.py
+++ portfolio/views/holding.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.http import HttpResponse
 
 from ..models import Holding
 from ..forms import HoldingForm
