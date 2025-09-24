a8cb52eff41fffd16ad7c4b4f0cf478aa345b820
gorozooo <coco.louis67@gmail.com>
2025-09-24 17:52:02 +0900

    holding.py を更新

--- portfolio/views/holding.py
+++ portfolio/views/holding.py
@@ -1,12 +1,13 @@
-# portfolio/views/holding.py
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
 from ..models import Holding
 from ..forms import HoldingForm
 
 @login_required
 def holding_list(request):
-    holdings = Holding.objects.filter(user=request.user).order_by("-opened_at", "-id")
+    holdings = Holding.objects.filter(user=request.user).order_by("-opened_at", "-updated_at", "-id")
     return render(request, "holdings/list.html", {"holdings": holdings})
 
 @login_required
@@ -14,22 +15,24 @@ def holding_create(request):
     if request.method == "POST":
         form = HoldingForm(request.POST)
         if form.is_valid():
-            holding = form.save(commit=False)
-            holding.user = request.user
-            holding.save()
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            messages.success(request, "保有を登録しました。")
             return redirect("holding_list")
     else:
         form = HoldingForm()
-    return render(request, "holdings/form.html", {"form": form})
+    return render(request, "holdings/form.html", {"form": form, "mode": "create"})
 
 @login_required
 def holding_edit(request, pk):
-    holding = get_object_or_404(Holding, pk=pk, user=request.user)
+    obj = get_object_or_404(Holding, pk=pk, user=request.user)
     if request.method == "POST":
-        form = HoldingForm(request.POST, instance=holding)
+        form = HoldingForm(request.POST, instance=obj)
         if form.is_valid():
             form.save()
+            messages.success(request, "保有を更新しました。")
             return redirect("holding_list")
     else:
-        form = HoldingForm(instance=holding)
-    return render(request, "holdings/form.html", {"form": form})
+        form = HoldingForm(instance=obj)
+    return render(request, "holdings/form.html", {"form": form, "mode": "edit", "obj": obj})
