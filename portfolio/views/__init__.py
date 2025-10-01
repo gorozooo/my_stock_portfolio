837ff6bfbdcae39469420dc74b1cc592d1d66af5
gorozooo <coco.louis67@gmail.com>
2025-10-01 22:32:46 +0900

    __init__.py を更新

--- portfolio/views/__init__.py
+++
@@ -1,5 +0,0 @@
-# portfolio/views/__init__.py
-# 既存の import はそのまま残しつつ、下記を追加してください
-
-from .calendar import dividends_calendar, dividends_calendar_json
-from .forecast import dividends_forecast, dividends_forecast_json
