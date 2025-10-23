6d4e2f718aa382ec8778cc62ad1819abc5033a83
gorozooo <coco.louis67@gmail.com>
2025-10-24 07:33:26 +0900

    scan_positions.py を更新

--- portfolio/management/commands/scan_positions.py
+++ portfolio/management/commands/scan_positions.py
@@ -1,8 +1,9 @@
+# portfolio/management/commands/scan_positions.py
 from django.core.management.base import BaseCommand
 from ...services.trader import evaluate_positions
 
 class Command(BaseCommand):
-    help = "保有中ポジションを監視してSTOP/TPを自動判定"
+    help = "保有中ポジションをスキャンしてSTOP/TP/クローズを判定し通知"
 
     def handle(self, *args, **options):
         self.stdout.write("Scanning open positions...")
