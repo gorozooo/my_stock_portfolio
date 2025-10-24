bc0206035c97cc262dd2bf5a42be9e6fad8ad4d1
gorozooo <coco.louis67@gmail.com>
2025-10-25 00:02:06 +0900

    urls.py を更新

--- advisor/urls.py
+++ advisor/urls.py
@@ -4,4 +4,7 @@ from advisor.views import page, api
 urlpatterns = [
     path("board/", page.board_page, name="advisor_board_page"),     # 画面
     path("api/board/", api.board_api, name="advisor_board_api"),    # JSON
+    path("api/action/", api.record_action, name="advisor_record_action"),      # ← 追加
+    path("api/remind/", api.create_reminder, name="advisor_create_reminder"),  # ← 追加
+
 ]
