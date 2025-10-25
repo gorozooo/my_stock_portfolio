a0625b01ee43ac2357e6cc57cd026564cb6eed42
gorozooo <coco.louis67@gmail.com>
2025-10-25 12:01:27 +0900

    urls.py を更新

--- advisor/urls.py
+++ advisor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from advisor.views import page, api
+from advisor.views import page, api, watch_api
 
 urlpatterns = [
     path("board/", page.board_page, name="advisor_board_page"),     # 画面
@@ -9,5 +9,8 @@ urlpatterns = [
     path("api/ping/", api.ping, name="advisor_ping"),
     path("api/debug_add/", api.debug_add, name="advisor_debug_add"),
     path("api/debug_add_reminder/", api.debug_add_reminder, name="advisor_debug_add_reminder"),
+    path("api/watch/list/", watch_api.watch_list, name="advisor_watch_list"),
+    path("api/watch/upsert/", watch_api.watch_upsert, name="advisor_watch_upsert"),
+    path("api/watch/archive/", watch_api.watch_archive, name="advisor_watch_archive"),
     
 ]
