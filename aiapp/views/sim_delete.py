5f0b07fbf38db5aa9ad09127efd37209053d17e4
gorozooo <coco.louis67@gmail.com>
2025-11-21 21:06:37 +0900

    sim_delete.py を更新

--- aiapp/views/sim_delete.py
+++ aiapp/views/sim_delete.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 
-from aiapp.models import AISimulation  # ★ シミュレ用モデル名（違っていたら教えて）
+from aiapp.models import VirtualTrade
 
 
 @login_required
@@ -21,11 +21,10 @@ def simulate_delete(request, pk: int, *args: Any, **kwargs: Any):
     - 削除後は一覧画面(simulate_list)に戻る
     - 「next」パラメータがあればそこを優先してリダイレクト
     """
-    sim = get_object_or_404(AISimulation, pk=pk)
-    sim.delete()
+    vt = get_object_or_404(VirtualTrade, pk=pk)
+    vt.delete()
 
     next_url = request.POST.get("next")
     if not next_url:
-        # 一覧ページに戻す（URL名は simulate_list を想定）
         return redirect("aiapp:simulate_list")
     return redirect(next_url)
