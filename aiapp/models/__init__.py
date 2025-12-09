2b0e32125b10e3033144f18354a4835da67f9f8f
gorozooo <coco.louis67@gmail.com>
2025-12-10 07:46:52 +0900

    __init__.py を更新

--- aiapp/models/__init__.py
+++ aiapp/models/__init__.py
@@ -1,7 +1,23 @@
+# aiapp/models/__init__.py
+
+# 既存モデル
 from .master import StockMaster
 from .vtrade import VirtualTrade
 
+# 新しく追加した “指数・為替・レジーム” モデル
+from .macro import (
+    BenchmarkMaster,
+    BenchmarkPrice,
+    MacroRegimeSnapshot,
+)
+
 __all__ = [
+    # 既存
     "StockMaster",
     "VirtualTrade",
+
+    # 新モデル
+    "BenchmarkMaster",
+    "BenchmarkPrice",
+    "MacroRegimeSnapshot",
 ]
