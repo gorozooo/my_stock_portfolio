4c9fb2697d14b328adaddb44d046798e71a2c9ba
gorozooo <coco.louis67@gmail.com>
2026-01-21 22:38:04 +0900

    __init__.py を更新

--- aiapp/services/daytrade/__init__.py
+++ aiapp/services/daytrade/__init__.py
@@ -1,1 +1,12 @@
-# aiapp/services/daytrade/__init__.py
+# -*- coding: utf-8 -*-
+"""
+ファイル: aiapp/services/daytrade/__init__.py
+
+これは何？
+- daytrade（デイトレ全自動）関連の「サービス層」をまとめるためのパッケージ初期化ファイルです。
+- 中身は空でもOKです（Pythonに「このフォルダはパッケージだよ」と知らせます）。
+
+置き場所（重要）
+- プロジェクトルート（manage.py がある階層）から見て:
+  aiapp/services/daytrade/__init__.py
+"""
