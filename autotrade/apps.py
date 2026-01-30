bebcf5bd4329d11a4d3ea38a78f30855bf185d8e
gorozooo <coco.louis67@gmail.com>
2026-01-30 22:36:16 +0900

    apps.py を作成

---
+++ autotrade/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AutotradeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "autotrade"
+    verbose_name = "Auto DayTrade"
