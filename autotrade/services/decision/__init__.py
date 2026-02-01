a033f64fbb4d29c63da9b78735a42a6b876f443c
gorozooo <coco.louis67@gmail.com>
2026-02-01 23:15:04 +0900

    __init__.py を作成

---
+++ autotrade/services/decision/__init__.py
@@ -0,0 +1,11 @@
+"""
+[FILE] autotrade/services/decision/__init__.py
+[PATH] <project_root>/autotrade/services/decision/__init__.py
+
+このファイルは何？
+- 「判断系ロジック」をまとめるパッケージの入口です。
+
+初心者ポイント：
+- 中身は空でOK。
+- フォルダをPythonモジュールとして扱うために必要です。
+"""
