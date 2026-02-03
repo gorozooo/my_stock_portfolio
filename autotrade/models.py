940047cf59b22dab4d8071fffcfef59f2607b640
gorozooo <coco.louis67@gmail.com>
2026-02-03 12:20:08 +0900

    models.py を更新

--- autotrade/models.py
+++ autotrade/models.py
@@ -201,4 +201,3 @@ class AutoTradeBacktestRun(models.Model):
     def __str__(self):
         return f"[BACKTEST] {self.snapshot.label} {self.gate_result}"
       
-from .models_backtest import AutoTradeExecution, AutoTradeBacktestRunDetail
