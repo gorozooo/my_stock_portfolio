192fdbcc0f935a3120e1d3987de1dacefbe50dc6
gorozooo <coco.louis67@gmail.com>
2026-02-11 22:05:46 +0900

    __init__.py を作成

---
+++ kakeibo/views/__init__.py
@@ -0,0 +1,10 @@
+# =========================================
+# [FILE] __init__.py
+# [PATH] kakeibo/views/__init__.py
+#
+# このファイルは何？
+# viewsフォルダをDjangoに認識させ、
+# dashboard関数を外部からimport可能にする。
+# =========================================
+
+from .dashboard import dashboard
