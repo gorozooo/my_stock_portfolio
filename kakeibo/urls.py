c1eaaea88e7a4bd309b3df9939175a34f3c7f90b
gorozooo <coco.louis67@gmail.com>
2026-02-13 18:15:13 +0900

    urls.py を更新

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -7,7 +7,13 @@
 # =========================================
 
 from django.urls import path
-from .views import dashboard, expense_create, income_create, settings_view
+from .views import (
+    dashboard,
+    expense_create,
+    income_create,
+    settings_view,
+    account_delete,
+)
 
 app_name = "kakeibo"
 
@@ -16,4 +22,5 @@ urlpatterns = [
     path("expense/", expense_create, name="expense_create"),
     path("income/", income_create, name="income_create"),
     path("settings/", settings_view, name="settings"),
+    path("settings/account/<int:pk>/delete/", account_delete, name="account_delete"),
 ]
