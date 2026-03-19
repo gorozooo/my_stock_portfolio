bfa98d4d09800bf396608f02b3c4383d0d11fe51
gorozooo <coco.louis67@gmail.com>
2026-03-19 20:31:06 +0900

    __init__.py を更新

--- kakeibo/models/__init__.py
+++ kakeibo/models/__init__.py
@@ -5,6 +5,10 @@
 # このファイルは何？
 # modelsフォルダをDjangoに認識させ、
 # 家計簿で使うモデルを一括importできるようにする。
+#
+# 今回の修正：
+# - ダッシュボード下部の「月ごとの自由メモ」用モデル
+#   MonthlyDashboardMemo を追加でimportする
 # =========================================
 
 from .account import Account
@@ -15,3 +19,4 @@ from .fixed_expense_template import FixedExpenseTemplate
 from .monthly_variable_expense import MonthlyVariableExpense
 from .bank_balance import BankBalance
 from .monthly_snapshot import MonthlySnapshot
+from .monthly_dashboard_memo import MonthlyDashboardMemo
